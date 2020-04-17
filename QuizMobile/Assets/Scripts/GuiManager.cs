aae19f9ffd9b6acfaea92120a055e3b47edfec5c
Konstantinos Giannopoulos <kostaswipe@gmail.com>
2020-04-17 16:26:19 +0300

    new ui assets and star rating calculation

--- QuizMobile/Assets/Scripts/GuiManager.cs
+++ QuizMobile/Assets/Scripts/GuiManager.cs
@@ -21,6 +21,9 @@ namespace Managers
         public GameObject[] livesIndicator;
         public GameObject StatsScreenCanvas;
 
+        public Text percent;
+        public Text percentNeeded;
+
         public Button[] Answers;
 
         private void Awake()
@@ -56,8 +59,33 @@ namespace Managers
         {
             CorrectAnswersScore.text = totalCorrectAnswers.ToString();
             TotalAnswersScore.text = "/" + totalQuestions.ToString();
-            
-            TotalQuetionsFiller.fillAmount = (float)qm._instance.answersGiven / (float)qm._instance.totalQuestions;
+
+            // calculate stars
+            float p = (float)qm._instance.totalCorrectAnswers / qm._instance.answersGiven * 100;
+            float pNeeded = (float)qm._instance.answersNeededToPassLevel / qm._instance.totalQuestions * 100;
+            percent.text = p.ToString("n2");
+            percentNeeded.text = pNeeded.ToString("n2");
+
+            if(p < pNeeded)
+            {
+                TotalQuetionsFiller.fillAmount = 0;
+            }
+            // one star
+            else if(p >= pNeeded && p <= 90)
+            {
+                TotalQuetionsFiller.fillAmount = 0.4f;
+            }
+            // two stars
+            else if(p >= pNeeded && p > 90 && p < 100)
+            {
+                TotalQuetionsFiller.fillAmount = 0.8f;
+            }
+            // 3 stars
+            else
+            {
+                TotalQuetionsFiller.fillAmount = 1;
+            }
+            //TotalQuetionsFiller.fillAmount = (float)qm._instance.totalCorrectAnswers / (float)qm._instance.totalQuestions;
         }
 
         public void UpdatePlayerScore(int score)
